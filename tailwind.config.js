07575962a136c68abf8ec34c9d64b93fe04e4a63
pedramjlo <pedram_sif@tuta.io>
2024-10-10 15:59:16 +0330

    set up css tw, added fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  
+
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
 
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "custom-black": "#1E201E",
+        "custom-white": "#FAF9F6",
+        "custom-blue": "#58B2FE"
+      },
+
+      fonts: {
+        "serif": ["El Messiri", "serif"],
+        "sans-serif": ["El Messiri", "sans-serif"],
+      }
+    },
   },
+
+
   plugins: [],
 }
